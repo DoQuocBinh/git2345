b7de394e519db92b819aa49f11658b812ba5f235
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-11-21 11:40:41 +0700

    aaa

--- index.php
+++ index.php
@@ -5,8 +5,20 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello Mr Cooollll!";
+echo "Hello Mr Cooollll!";	
 ?>
+<input type="text"  id="name">
+<br>
+<input type="text"  id="school">
+<br>
+<input type="button" value="Ok" onclick="display()">
 
+<script type="text/javascript">
+	function display(){
+		var name = document.getElementById('name').value;
+		var school = document.getElementById('school').value;
+		alert('welcome ' + name + ' from school ' + school);
+	}
+</script>
 </body>
 </html>
