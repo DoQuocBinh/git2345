7c3c03a7651cb0a39cb5c1d84903736f949c4bbb
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-11-22 18:36:11 +0700

    small change

--- index.php
+++ index.php
@@ -14,7 +14,14 @@ echo "Hello Mr Cooollll!";
 <input type="button" value="Ok" onclick="display()">
 
 <script type="text/javascript">
+	function checkInput(){
+		var name = document.getElementById('name').value;
+		if (name.length <5) {
+			alert('Name is less than 5 characters');
+		}
+	}
 	function display(){
+		checkInput();
 		var name = document.getElementById('name').value;
 		var school = document.getElementById('school').value;
 		alert('welcome ' + name + ' from school ' + school);
