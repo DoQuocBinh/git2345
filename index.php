a4e1ab966bf8f2255fe3eba7101c36a51b7dec90
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-11-28 08:45:20 +0700

    insert function

--- index.php
+++ index.php
@@ -1,31 +1,14 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<h1>Example page </h1>
-
-<?php
-	echo "Hello Mr Cooollll!";	
-?>
-<input type="text"  id="name">
-<br>
-<input type="text"  id="school">
-<br>	
-<input type="button" value="Ok" onclick="display()">
-
-<script type="text/javascript">
-	function checkInput(){
-		var name = document.getElementById('name').value;
-		if (name.length <8) {
-			alert('Name is less than 8 characters');
-		}
-	}
-	function display(){
-		checkInput();
-		var name = document.getElementById('name').value;
-		var school = document.getElementById('school').value;
-		alert('welcome ' + name + ' from school ' + school);
-	}
-</script>
+	<h1>Create Product </h1>
+	<form action="insertProduct.php" method="post">
+		Name <input type="text" name="name" id="name">
+		<br>
+		Price <input type="text" name="price"  id="price">
+		<br>
+		<input type="submit" value="Insert">
+	</form>
+	
 </body>
 </html>
